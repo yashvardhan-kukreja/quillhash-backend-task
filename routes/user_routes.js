999c0c8fc8aae6378c0c9fe5b5dcd5e33b97df5d
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2019-04-02 12:15:59 +0530

    Route: Profile check other user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -11,9 +11,19 @@ router.use((req, res, next) => {
         .catch(err => res.status(err.meta.code).json(err));
 });
 
-router.get("/my-profile", (req, res) => {
+router.get("/profile/me", (req, res) => {
     let user_id = req.decoded._id;
-    UserController.fetch_my_profile(user_id)
+    UserController.fetch_profile(user_id)
+        .then(data => res.status(data.meta.code).json(data))
+        .catch(err => res.status(err.meta.code).json(err));
+});
+
+router.get("/profile", (req, res) => {
+    let blocked_or_unblocked_userid = req.decoded._id; // currently logged in user
+    let main_user_id = req.query.id; // user whose profile needs to be seen
+
+    UserController.check_block_status(main_user_id, blocked_or_unblocked_userid)
+        .then(data => UserController.fetch_profile(main_user_id))
         .then(data => res.status(data.meta.code).json(data))
         .catch(err => res.status(err.meta.code).json(err));
 });
