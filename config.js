5160d7eccfc34404908508e36eaf345d5bc3909f
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2019-04-02 19:49:30 +0530

    added cloudinary support

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 module.exports = {
     PRODUCTION_DB: "mongodb://yash98:quillhash1234@ds251518.mlab.com:51518/quillhash-task",
     TEST_DB: "mongodb://yash98:internet92@ds153093.mlab.com:53093/quillhash-task-testdb",
-    JWT_SECRET: "earthisflat1234321"
+    JWT_SECRET: "earthisflat1234321",
+    cloudinary_config: {
+        CLOUD_NAME: "dovxlbprt",
+        API_KEY: "538892362375798",
+        API_SECRET: "IBDOt6diOuxhSom0cxrjel5ZyHs"
+    }
 }
